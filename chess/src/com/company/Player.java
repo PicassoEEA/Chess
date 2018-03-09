b25f0d42a0edac3f28603e1b1ea632dd6c469009
PicassoEEA <bi.yihe@qq.com>
2018-03-09 13:10:09 +0800

    modified player class

--- chess/src/com/company/Player.java
+++ chess/src/com/company/Player.java
@@ -23,5 +23,28 @@ public class Player {
         return name;
     }
 
-    private int[][] Str2Index(){}
+    public void move(String startPosition, String endPosition){
+
+    }
+
+    private int[] Str2Index(String str){
+        char char1 = str.substring(0,1).toCharArray()[0];
+        char char2 = str.substring(1,2).toCharArray()[0];
+        int index1 = 0;
+        int index2 = 0;
+
+        if (char1 >= 97){
+            char1 -= 32;
+        }
+
+
+        index1 = ((int) char1) - 65;
+        index2 = ((int) char2) - 48 - 1;
+
+        int[] output = new int[2];
+        output[0] = index1;
+        output[1] = index2;
+
+        return output;
+    }
 }
