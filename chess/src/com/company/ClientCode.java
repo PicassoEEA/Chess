3b0e3dc434854fe8b0d456de4a88e64c784f4c34
Jeremy113 <jieying_xi@126.com>
2018-03-23 13:59:15 +0800

    j

--- chess/src/com/company/ClientCode.java
+++ chess/src/com/company/ClientCode.java
@@ -1,5 +1,37 @@
 package com.company;
-
+import java.util.Scanner;
 public class ClientCode {
-    2
+    static Scanner scanner = new Scanner(System.in);
+    static Player whitePlayer = null;
+    static Player blackPlayer = null;
+    static Board board = new Board();
+
+    public static void main(String[] args) {//Jeremy created at 3.23 1:12
+        System.out.print("please enter the White player's name : ");
+        whitePlayer = Player.createPlayer(scanner.nextLine());
+        System.out.println();
+        System.out.print("please enter the Black player's name : ");
+        blackPlayer = Player.createPlayer(scanner.nextLine());
+
+        String inputLine = "";
+        while(!(inputLine.equals("quit"))){
+            System.out.println(board);
+            System.out.print(whitePlayer + "'s move : ");
+
+        }
+
+    }
+    public static String[] seperater(String input){
+        String[] seperateString = new String[2];
+        int positionOfcomma = 0;
+        for(int i = 0; i < input.length(); i ++){
+            if(input.substring(i,i + 1) == ","){
+                 seperateString[positionOfcomma ] = input.substring(positionOfcomma );
+                 seperateString[positionOfcomma + 1 ] = input.substring(positionOfcomma + 1,input.length() );
+            }
+
+        }
+        return seperateString;
+    }
+
 }
